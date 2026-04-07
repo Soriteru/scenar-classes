dc08007616ca3966f1892d6257b32529776fe1f7
Soriteru <sohanblemandpro@gmail.com>
2026-04-07 15:08:11 +0200

    Mission 4

--- ClassesJava/src/classesjava/ClassesJava.java
+++ ClassesJava/src/classesjava/ClassesJava.java
@@ -14,12 +14,25 @@ public class ClassesJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String marque = "Peugeot";
-        String modele = "208";
-        int annee = 2021;
-        int prix = 14500;
+        Voiture v1 = new Voiture();
+        v1.marque = "Peugeot";
+        v1.modele = "208";
+        v1.annee = 2021;
+        v1.prix = 14500;
         
-        System.out.println("La voiture est une "+marque+" "+modele+" datant de "+annee+" au prix de "+prix+"€.");
+        Voiture v2 = new Voiture();
+        v2.marque = "Renault";
+        v2.modele = "Clio";
+        v2.annee = 2000;
+        v2.prix = 3500;
+        
+        Voiture v3 = new Voiture();
+        v3.marque = "Toyota";
+        v3.modele = "Yaris";
+        v3.annee = 2015;
+        v3.prix = 9800;
+        
+        System.out.println("Test d'affichage :");
+        System.out.println("La voiture "+v3.marque+" "+v3.modele+" de l'année "+v3.annee+" au prix de : "+v3.prix+" euros.");
     }
-    
 }
