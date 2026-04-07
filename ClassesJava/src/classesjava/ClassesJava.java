b0eadcf05650c21ac7aeaafcdbc912b587beae43
Soriteru <sohanblemandpro@gmail.com>
2026-04-07 14:11:54 +0200

    Mission 1 et 2

---
+++ ClassesJava/src/classesjava/ClassesJava.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package classesjava;
+
+/**
+ *
+ * @author sohan
+ */
+public class ClassesJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String marque = "Peugeot";
+        String modele = "208";
+        int annee = 2021;
+        int prix = 14500;
+        
+        System.out.println("La voiture est une "+marque+" "+modele+" datant de "+annee+" au prix de "+prix+"€.");
+    }
+    
+}
